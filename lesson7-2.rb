a9a6c1a2d622cde15c08ec3d118019eb5680b238
EC2 Default User <ec2-user@ip-172-31-90-209.ec2.internal>
2020-12-27 06:38:03 +0000

    7-2[Finsh]

--- lesson7-2.rb
+++ lesson7-2.rb
@@ -1,22 +1,25 @@
 puts "計算を始めます"
 puts "何回計算を繰り返しますか？"
-times = gets.to_i
-number = 0
 
-while times != number do
-  number = number + 1
+times = gets.to_i
+number = 1
+
+while number <= times do
   puts "#{number}回目の計算"
   puts "2つの値を入力してください"
+
   a = gets.to_i
   b = gets.to_i
+
   puts "a=#{a}"
   puts "b=#{b}"
+
   puts "計算結果を出力します"
   puts "a+b=#{a+b}"
   puts "a-b=#{a-b}"
   puts "a*b=#{a*b}"
-  if times == number
-    puts "計算を終了します"
-    break
-  end
+
+  number += 1
 end
+
+puts "計算を出力する"
